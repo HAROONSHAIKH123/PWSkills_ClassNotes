1bf5192c0ccbad9c20f0da7814c89df7bbcb4638
HAROONSHAIKH123 <hrrnshaikh@gmail.com>
2023-02-21 01:03:49 +0530

    update

---
+++ CLASS/01_PYTHON/02_20_Git, Flask Http Methods, Templates, Project Postman/git.py
@@ -0,0 +1,18 @@
+# to push code into git
+'''
+echo "# aaaaaaaaaa" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/HAROONSHAIKH123/aaaaaaaaaa.git
+git push -u origin main
+
+
+git remove remote origin
+'''
+
+# to clone git repo
+'''
+git clone "past git url"
+'''
